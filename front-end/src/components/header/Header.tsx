87694884779421ad7069f03edc75213d317fcad3
Weslei Medeiros da Rosa <139824516+DevWesleyMedeiros@users.noreply.github.com>
2026-05-23 17:47:29 -0300

    chore(front-end) instalação da Lib. em devDependencie prettier-plugin-tailwindcss

--- front-end/src/components/header/Header.tsx
+++ front-end/src/components/header/Header.tsx
@@ -1,16 +1,16 @@
 export const Header = () => {
   return (
     <div className="bg-[#161410]">
-      <div className="w-full md:w-184.25 p-3 md:p-0 mx-auto flex items-center justify-between">
+      <div className="mx-auto flex w-full items-center justify-between p-3 md:w-184.25 md:p-0">
         <img src="./logo.png" alt="Logo link caso do Hamburger" />
         <button
           type="button"
           title="Entrar"
-          className="bg-[#F2DAAC] w-32.5 h-8.75 rounded-md flex items-center justify-center cursor-pointer"
+          className="flex h-8.75 w-32.5 cursor-pointer items-center justify-center rounded-md bg-[#F2DAAC]"
         >
           Entrar
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
