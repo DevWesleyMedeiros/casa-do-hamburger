f1b15af7c47a6c878f7189bdae92047608404151
Weslei Medeiros da Rosa <139824516+DevWesleyMedeiros@users.noreply.github.com>
2026-05-25 10:33:45 -0300

    feat(frontend-UI): adiciona layout com Header e Outlet para rotas compartilhadas

--- front-end/src/shared/routes/AppRoutes.tsx
+++ front-end/src/shared/routes/AppRoutes.tsx
@@ -1,8 +1,24 @@
 import { Login } from "../../pages/login/Login";
 import { Register } from "../../pages/register/Register";
 import Home from "../../pages/home/Home";
+import { Header } from "../../components/header/Header";
 
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+
+const Layout: React.FC = () => {
+  return (
+    <>
+      <Header></Header>
+      <Outlet />
+    </>
+  );
+};
 
 export const AppRoutes = () => {
   return (
@@ -11,8 +27,10 @@ export const AppRoutes = () => {
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
     </BrowserRouter>
   );
