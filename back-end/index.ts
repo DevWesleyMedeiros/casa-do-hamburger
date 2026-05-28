9e406764243acaa4cc9ad98cd9916c427a866a9e
Weslei Medeiros da Rosa <139824516+DevWesleyMedeiros@users.noreply.github.com>
2026-05-28 11:06:06 -0300

    chore(backend): add Prisma ORM and PostgreSQL setup

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,10 +1,17 @@
 // arquivo inicial padrão do projeto
 import express from 'express'
 import cors from 'cors'
+import { connection } from './src/db'
 
 const app = express()
+connection()
 
 app.use(cors())
+
+// método http get
+app.get('/teste', (req, res) => {
+  res.json('Você acessou a rota teste')
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
---
+++ back-end/src/db.ts
@@ -0,0 +1,21 @@
+// arquivo que conecta meu cliente prisma com meu backend
+
+import 'dotenv/config'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
+
+const connectionString = `${process.env['DATABASE_URL']}`
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+// forma antiga de usar o prisma
+// const prisma = new PrismaClient()
+
+export { prisma }
+
+// função que cria uma conexão com meu BD
+export async function connection() {
+  await prisma.$connect()
+  console.log('Conectado com o meu Banco de Dados')
+}
