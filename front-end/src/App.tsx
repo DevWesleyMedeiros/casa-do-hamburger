10726e49656925c067cc9a1e95a3276b371cc5d5
Weslei Medeiros da Rosa <139824516+DevWesleyMedeiros@users.noreply.github.com>
2026-07-05 12:16:36 -0300

    refactor(App): wrap application in BrowserRouter and remove FetchCartItems component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,19 +1,18 @@
 import { Toaster } from "sonner";
 import "./App.css";
-import { FetchCartItems } from "./shared/components/FetchCartItem";
 import { FetchUser } from "./shared/components/FetchUser";
 import { AppRoutes } from "./shared/routes/AppRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Toaster position="top-center" richColors duration={3000} />
 
       {/* Disparamos a função UMA ÚNICA VEZ quando a aplicação é montada */}
       <FetchUser />
-      <FetchCartItems />
       <AppRoutes></AppRoutes>
-    </>
+    </BrowserRouter>
   );
 }
 export default App;
