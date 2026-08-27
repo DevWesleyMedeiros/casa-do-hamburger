ac176eadcc8f76b8142cdeffa913c7517b6fbcb9
Weslei Medeiros da Rosa <139824516+DevWesleyMedeiros@users.noreply.github.com>
2026-08-26 22:46:44 -0300

    fix(resetPassword-api): fix import from name error on resetPassword api

--- front-end/src/shared/services/api/recovery/resetPassword.ts
+++ front-end/src/shared/services/api/recovery/resetPassword.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { type AuthMessageResponse } from "../../../../types/RecoveryPass";
+import { type AuthMessageResponse } from "../../../../types/recoveryPass";
 import { api } from "../ApiConfig";
 import { ApiError } from "../ApiExceptions";
 
