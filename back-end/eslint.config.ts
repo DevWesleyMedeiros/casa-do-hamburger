629ee932a1454f7485e401a9fa86e2d1cca4681d
Weslei Medeiros da Rosa <139824516+DevWesleyMedeiros@users.noreply.github.com>
2026-09-19 17:55:23 -0300

    refactor(eslint): update configuration to use defineConfig and allow statement log as a new rule

--- back-end/eslint.config.ts
+++ back-end/eslint.config.ts
@@ -4,11 +4,12 @@
 // Simula as regras que um time sênior aplicaria em PR review automatizado.
 
 import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
 import security from 'eslint-plugin-security'
+import { defineConfig } from 'eslint/config'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default defineConfig([
   // 1) O que o lint NUNCA deve tocar: gerado por ferramenta, build, deps
   {
     ignores: [
@@ -44,7 +45,7 @@ export default tseslint.config(
       eqeqeq: ['error', 'always'], // proíbe == , exige ===
       'no-var': 'error', // força let/const
       'prefer-const': 'error',
-      'no-console': ['warn', { allow: ['warn', 'error'] }], // console.log solto vira warning
+      'no-console': ['warn', { allow: ['warn', 'error', 'log'] }], // console.log solto vira warning
 
       // --- TypeScript ---
       '@typescript-eslint/no-unused-vars': [
@@ -76,4 +77,4 @@ export default tseslint.config(
       'no-console': 'off',
     },
   },
-)
+])
